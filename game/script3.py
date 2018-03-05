38eb7f98f9dbef57393bbbf5c156cb7dae51b6b3
Grace Chen <gracechen.sd@gmail.com>
2018-03-05 02:37:46 -0800

    Draft for scene 3

--- game/script3.py
+++ game/script3.py
@@ -1,13 +1,9 @@
-
-
+# NOTE
+# Replace label end2 and onwards in script.rpy with this.
 label scene3start:
     scene black with fade
     
     ""
-
-   # Show a background. This uses a placeholder by default, but you can
-    # add a file (named either "bg room.png" or "bg room.jpg") to the
-    # images directory to show it.
 
     scene girlfriend crying at truecenter with dissolve
 
@@ -88,30 +84,60 @@ label final:
 
     "My heart palpitations are stronger than ever this morning..."
     "Shivering furiously, I climb out of bed and dig through my refrigerator for something to eat."
-    "Sitting on my bed, I spend the entire day drinking once again."
+    "Sitting on my bed, I spend the entire day drinking again."
 
     scene morning at truecenter
     with fade
 
-    "I have a horrible hangover this morning"
+    "I have a horrible hangover this morning."
+    "Despite all that, I'm shivering so hard I can't focus on anything at all."
+    "I spend the entire day drinking once again."
 
     scene morning at truecenter
     with fade
 
     "Ring! Riiing!"
-    "I'm woken up in the middle of the night by a phone call."
+    "A phone call? In the middle of the night?"
+    "I'm tempted to ignore it, but I immediately notice the name on the screen - it's the doctor. I saved the number from the last time Scott was hospitalized."
+    m "Doctor? Is everything alright?"
+    d "Apologies for calling so late at night. It's about your friend - Scott."
+    "I immediately feel a cold rush through my veins and a shiver down my back. What's happened?"
+    d "I'm so sorry but he's been in a terrible car accident. Both he and the other driver were drunk."
+    d "We don't have contact information for any of his relatives, only you."
+    d "We thought you'd like to know and we think you should see him."
+    "Before he could even finish talking, I immediately jump out of bed and get in my car to rush to the hospital."
+
+    scene hospital at truecenter
+    with fade
+
+    "I wait outside for several hours while they perform the necessary surgery on him."
+    "I had never felt so worried in my life."
+    "After a while, the doctors come out and inform me of his status."
+    "I'm relieved that he only suffered a broken leg and a minor head injury. I know it could have been much worse."
+    "I go in to see him. He greets me with a crooked smile."
+    s "Sorry you had to come in and see me like this again."
+    s "I'm feeling much better now though. I'm so glad I'm still alive."
+    s "Guess I really should do something about that drinking problem, huh?"
+    m "I'm glad too, bro. And I think we both need some help in that department."
+    "He nodded and I could see that we both knew what we needed to do."
 
     
     scene hands at truecenter
     with fade
     
-    "I forced myself to ask for help at my local Drug and Alcohol Team unit."
-    
+    "When Scott got out of the hospital, we both started looking into getting help together."
+    "My girlfriend and family were incredibly supportive. They started looking into all sorts of resources for our sake."
+    "The two of us eventually got help at our local Drug and Alcohol Team unit."
     "They were incredibly supportive and understood exactly what I was going through."
-    "They had me spend 10 days in the hospital to detox me."
+    "The doctor helped talk Scott through methods for avoiding binge drinking."
+    "As for me, they had me spend 10 days in the hospital to detox me."
+    "During that time, I struggled each day to make it through the day with all the heart palpitations and shivering that come with withdrawal."
     "The doctor explained that I was simply one of those people that was “allergic” to alcohol, that for me, alcohol is poison, and that I will never be able to control alcohol as long as I live."
     "I understand now that I was fighting a losing battle this entire time."
     "I was just not like most people when it comes to alcohol, and as long as I understood and worked with that, there was hope that I could move on."
+    "When I went home again, it was tough, but I started putting the doctor's advice to work to combat my addiction."
+    "Day by day, I could feel myself getting a little stronger. I was more like my old self."
+    "I think Scott, my girlfriend, and my family could all see that as well."
     "From here on out, I have the support I need to start rebuilding my life."
 
     # This ends the game.
